0f00547ee1030b4a3c94936dfa7e77164374a05c
Christopher Miller <admin@mctherealm.net>
2021-01-29 15:03:30 -0500

    Implemented Example Network Scene

---
+++ Assets/Scripts/Networking/GameConnectionSystem.cs
@@ -0,0 +1,44 @@
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using UnityEngine;
+
+[UpdateInWorld(UpdateInWorld.TargetWorld.Default)]
+public class GameConnectionSystem : ComponentSystem {
+  public struct InitGameComponent : ICommandData {
+    public uint Tick {get; set;}
+  }
+
+  protected override void OnCreate() {
+    RequireSingletonForUpdate<InitGameComponent>();
+    EntityManager.CreateEntity(typeof(InitGameComponent));
+  }
+
+  protected override void OnUpdate() {
+    EntityManager.DestroyEntity(GetSingletonEntity<InitGameComponent>());
+
+    foreach (var world in World.All) {
+      var network = world.GetExistingSystem<NetworkStreamReceiveSystem>();
+      if (world.GetExistingSystem<ClientSimulationSystemGroup>() != null) {
+        UnityEngine.Debug.Log("Starting up Client");
+        // Client localhost Connection
+        NetworkEndPoint ep = NetworkEndPoint.LoopbackIpv4;
+        ep.Port = 7979;
+        network.Connect(ep);
+      }
+      #if UNITY_EDITOR
+      else if (world.GetExistingSystem<ServerSimulationSystemGroup>() != null) {
+        // Server localhost listen
+        NetworkEndPoint ep = NetworkEndPoint.AnyIpv4;
+        ep.Port = 7979;
+
+        Debug.Log("Server is listening on port 7979");
+
+        network.Listen(ep);
+      }
+      #endif
+    }
+  }
+}
+
+public struct GameConnectRequest : IRpcCommand {}
