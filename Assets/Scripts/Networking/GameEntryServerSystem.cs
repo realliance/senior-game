ec4e1e86e2ce1bf28aca0a8e63a1f3bbaa0cff6f
Alice Hartley <alicehartleyhartley@gmail.com>
2021-02-09 03:38:49 -0500

    Adjusted Spawn Position of Players for easier testing

--- Assets/Scripts/Networking/GameEntryServerSystem.cs
+++ Assets/Scripts/Networking/GameEntryServerSystem.cs
@@ -1,5 +1,7 @@
 using System;
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
 using Unity.NetCode;
 using UnityEngine;
 
@@ -9,12 +11,15 @@ public class GameEntryServerSystem : ComponentSystem {
     Entities.WithNone<SendRpcCommandRequestComponent>().ForEach((Entity reqEnt, ref GameConnectRequest req, ref ReceiveRpcCommandRequestComponent reqSrc) => {
       PostUpdateCommands.AddComponent<NetworkStreamInGame>(reqSrc.SourceConnection);
 
-      Debug.Log(String.Format("Server setting connection {0} to in game", EntityManager.GetComponentData<NetworkIdComponent>(reqSrc.SourceConnection).Value));
+      var networkId = EntityManager.GetComponentData<NetworkIdComponent>(reqSrc.SourceConnection).Value;
+      Debug.Log(String.Format("Server setting connection {0} to in game", networkId));
+
 
       var spawnDataSingleton = GetSingletonEntity<PlayerSpawnData>();
       var spawnData = EntityManager.GetComponentData<PlayerSpawnData>(spawnDataSingleton);
       var player = EntityManager.Instantiate(spawnData.playerPrefab);
-      EntityManager.SetComponentData(player, new GhostOwnerComponent { NetworkId = EntityManager.GetComponentData<NetworkIdComponent>(reqSrc.SourceConnection).Value });
+      EntityManager.SetComponentData(player, new GhostOwnerComponent { NetworkId = networkId });
+      EntityManager.SetComponentData(player, new Translation { Value = new float3(Mathf.Sin(networkId)/2, 4, Mathf.Cos(networkId)/2) });
 
       PostUpdateCommands.AddBuffer<PlayerInput>(player);
       PostUpdateCommands.SetComponent(reqSrc.SourceConnection, new CommandTargetComponent { targetEntity = player });
