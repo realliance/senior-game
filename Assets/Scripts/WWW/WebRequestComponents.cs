515b42ff15158db9468c9afcfe879d2d0c626a3b
Christopher Miller <admin@mctherealm.net>
2021-02-10 18:59:17 -0500

    Added Web Request ECS Framework

---
+++ Assets/Scripts/WWW/WebRequestComponents.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using Unity.Collections;
+
+public enum WebVerb {
+  GET = 0,
+  POST = 1
+}
+
+public struct WebRequestComponent : IComponentData {
+  public FixedString128 requestURL;
+  public WebVerb verb;
+}
+
+public struct WebRequestParameter : IBufferElementData {
+  public FixedString64 name;
+  public FixedString64 value;
+}
+
+public struct WebResponse : IComponentData {
+  public long status;
+  public FixedString4096 response;
+}
