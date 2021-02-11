6e689d4e596cb4413031c3814b2060fb5098e305
Christopher Miller <admin@mctherealm.net>
2021-02-11 09:58:14 -0500

    Converted Global Application State to System-based Creation

---
+++ Assets/Scripts/Global/ApplicationStateCreationSystem.cs
@@ -0,0 +1,21 @@
+using Unity.Entities;
+using Unity.NetCode;
+
+[UpdateInWorld(UpdateInWorld.TargetWorld.Default)]
+public class ApplicationStateCreationSystem : SystemBase {
+  internal struct CreateApplicationStateSingleton : IComponentData {}
+
+  protected override void OnCreate() {
+    base.OnCreate();
+
+    RequireSingletonForUpdate<CreateApplicationStateSingleton>();
+    EntityManager.CreateEntity(typeof(CreateApplicationStateSingleton));
+  }
+
+  protected override void OnUpdate() {
+    EntityManager.DestroyEntity(GetSingletonEntity<CreateApplicationStateSingleton>());
+
+    var entity = EntityManager.CreateEntity(typeof(UserAccountStateComponent));
+    EntityManager.SetName(entity, "Global Application State");
+  }
+}
