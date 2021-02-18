6b98bb7061ed19b201c22664d03e976653a0fe33
ConnorJC <ccatlett2000@gmail.com>
2021-02-18 16:56:36 -0500

    Send unity to hell

--- Assets/Scripts/Global/ApplicationStateComponent.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Collections;
-using Unity.Entities;
-
-public struct UserAccountStateComponent : IComponentData {
-  public bool loggedIn;
-  public FixedString32 token;
-}
--- Assets/Scripts/Networking/PlayerInputCommandData.cs
+++
@@ -1,7 +0,0 @@
-using Unity.NetCode;
-
-public struct PlayerInput : ICommandData {
-  public uint Tick { get; set; }
-  public int horizontal;
-  public int vertical;
-}
