d92ec6e7ad1f39e0d61134fd387db9e409a1637e
ConnorJC <ccatlett2000@gmail.com>
2021-02-26 23:59:21 -0500

    Windows Sucks

--- build-windows-client.sh
+++ build-windows-client.sh
@@ -9,7 +9,9 @@ cargo install --locked --root install \
 mkdir -p windows
 cp install/bin/senior_game_client.exe windows/WizardConnect3.exe
 cp -r senior_game_client/assets windows
-cp /usr/x86_64-w64-mingw32/bin/{libstdc++-6.dll,libgcc_s_seh-1.dll,libwinpthread-1.dll} windows
+cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libstdc++-6.dll windows
+cp /usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/libgcc_s_seh-1.dll windows
+cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll windows
 
 cd windows
 mkdir -p ../clients/$VERSION
